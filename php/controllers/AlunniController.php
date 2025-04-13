57a2915fc8cd25003382868801a098a8d997ba85
cosimo <ballerini.co@gmail.com>
2025-04-13 22:58:04 +0200

    pronto 2

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -24,67 +24,59 @@ class AlunniController
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
 
-
   public function create(Request $request, Response $response, $args)
   {
-    $body=json_decode($request->getBody()->getContents(), true);
-    $nome=$body["nome"];
-    $cognome=$body["cognome"];
+    $body = json_decode($request->getBody()->getContents(), true);
+    $nome = $body["nome"];
+    $cognome = $body["cognome"];
 
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $result = $mysqli_connection->query("INSERT INTO alunni (nome, cognome) VALUES ('$nome', '$cognome')");
-    if($mysqli_connection->affected_rows>0)
-    {
-      $results=["msg"=>"ok"];
+    if ($mysqli_connection->affected_rows > 0) {
+      $results = ["msg" => "ok"];
       $response->getBody()->write(json_encode($results));
       return $response->withHeader("Content-type", "application/json")->withStatus(201);
     }
-    $results=["msg"=>"ko"];
+    $results = ["msg" => "ko"];
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(400);
-    
   }
 
   public function update(Request $request, Response $response, $args)
   {
-    $body=json_decode($request->getBody()->getContents(), true);
-    $nome=$body["nome"];
-    $cognome=$body["cognome"];
+    $body = json_decode($request->getBody()->getContents(), true);
+    $nome = $body["nome"];
+    $cognome = $body["cognome"];
     $mioId = $args["id"];
 
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
-    $result = $mysqli_connection->query("UPDATE alunni (nome, cognome) VALUES ('$nome', '$cognome') WHERE id =" . $mioId);
-    if($mysqli_connection->affected_rows>0)
-    {
-      $results=["msg"=>"ok"];
+    $result = $mysqli_connection->query("UPDATE alunni SET nome = '$nome', cognome = '$cognome' WHERE id = " . $mioId);
+    if ($mysqli_connection->affected_rows > 0) {
+      $results = ["msg" => "ok"];
       $response->getBody()->write(json_encode($results));
-      return $response->withHeader("Content-type", "application/json")->withStatus(201);
+      return $response->withHeader("Content-type", "application/json")->withStatus(200);
     }
-    $results=["msg"=>"ko"];
+    $results = ["msg" => "ko"];
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(400);
-    
   }
 
   public function delete(Request $request, Response $response, $args)
   {
-    $body=json_decode($request->getBody()->getContents(), true);
-    $nome=$body["nome"];
-    $cognome=$body["cognome"];
+    $body = json_decode($request->getBody()->getContents(), true);
+    $nome = $body["nome"];
+    $cognome = $body["cognome"];
     $mioId = $args["id"];
 
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     $result = $mysqli_connection->query("DELETE FROM alunni WHERE id =" . $mioId);
-    if($mysqli_connection->affected_rows>0)
-    {
-      $results=["msg"=>"ok"];
+    if ($mysqli_connection->affected_rows > 0) {
+      $results = ["msg" => "ok"];
       $response->getBody()->write(json_encode($results));
-      return $response->withHeader("Content-type", "application/json")->withStatus(201);
+      return $response->withHeader("Content-type", "application/json")->withStatus(200);
     }
-    $results=["msg"=>"ko"];
+    $results = ["msg" => "ko"];
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(400);
-    
   }
-
 }
